f28e4c1c32eeb47a7e93cd237c5dc41940a63d6e
Guy Bowerman <guybo@outlook.com>
2017-09-05 15:56:42 -0700

    Formatting

--- subscription.py
+++ subscription.py
@@ -1,3 +1,4 @@
+'''subscription.py - subscription class for basic subscription level operations'''
 import json
 
 import azurerm
@@ -5,24 +6,26 @@ import azurerm
 
 # Azure subscription class
 class subscription():
+    '''basic subscription level operations for VMSS Editor'''
     def __init__(self, tenant_id, app_id, app_secret, subscription_id):
         self.sub_id = subscription_id
         self.tenant_id = tenant_id
         self.app_id = app_id
         self.app_secret = app_secret
+        self.vmsslist = []
+        self.vmssdict = {}
+        self.status = ""
 
         self.access_token = azurerm.get_access_token(tenant_id, app_id, app_secret)
 
-    # update the authentication token for this subscription
     def auth(self):
+        '''update the authentication token for this subscription'''
         self.access_token = azurerm.get_access_token(self.tenant_id, self.app_id, self.app_secret)
         return self.access_token
 
-    # list VM Scale Sets in this subscription - names only
     def get_vmss_list(self):
+        '''list VM Scale Sets in this subscription - names only'''
         vmss_sub_list = azurerm.list_vmss_sub(self.access_token, self.sub_id)
-        self.vmsslist = []
-        self.vmssdict = {}
         # build a simple list of VM Scale Set names and a dictionary of VMSS model views
         try:
             for vmss in vmss_sub_list['value']:
